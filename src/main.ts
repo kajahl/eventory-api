be555a396fba5fb06854e3c63ddc48c757b57d7f
kajahl <kacperjahl37@gmail.com>
2025-03-25 00:30:15 +0100

    feat(main.ts): add cors, cookie parser, sessions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(
+        cors({
+            origin: '*',
+            credentials: true,
+        }),
+    );
+    app.use(cookieParser());
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET ?? 'my-secret',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 24 * 60 * 60 * 1000,
+            },
+        }),
+    );
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
