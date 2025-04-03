8bec174054e63ada8d3bcea34c1ef1dba2e39cf6
kajahl <kacperjahl37@gmail.com>
2025-04-04 01:41:38 +0200

    feat(users.controller): split endpoints to /id/:id and /me

--- src/modules/users/controllers/users/users.controller.ts
+++ src/modules/users/controllers/users/users.controller.ts
@@ -1,26 +1,49 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Inject, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, ForbiddenException, Get, Inject, Param, Patch, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UsersService } from '../../services/users/users.service';
 import CreateUserDto from '../../dto/CreateUser.dto';
 import UpdateUserDto from '../../dto/UpdateUser.dto';
+import { AccessJwtPassportAuthGuard } from 'src/modules/auth/guards/AccessJwtPassport.guard';
+import { ScopeAccessGuard } from 'src/modules/roles/guards/ScopeAccess.guard';
+import { Scopes } from 'src/modules/roles/decorators/Scopes.decorator';
+import { Scope } from 'src/modules/roles/types';
+import { User } from 'src/common/decorators/User.decorator';
+import { SignInData } from 'src/modules/auth/types';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AccessJwtPassportAuthGuard, ScopeAccessGuard) // JWT first
 export class UsersController {
     constructor(
         @Inject(UsersService) private readonly usersService: UsersService,
     ) {}
 
+    // Get All
+
     @Get()
+    @Scopes(Scope.UsersRead)
     getUsers() {
         return this.usersService.getAll();
     }
 
+    // Get User
+
+    @Get('me')
+    @Scopes(Scope.UsersReadMe)
+    getMe(
+        @User() user: SignInData,
+    ) {
+        return this.usersService.getById(user.userId);
+    }
+
     @Get('id/:id')
+    @Scopes(Scope.UsersRead)
     getUserById(
         @Param('id') id: string,
     ) {
         return this.usersService.getById(id)
     }
+
+    // Create
 
     @Post('create')
     createUser(
@@ -29,19 +52,43 @@ export class UsersController {
         return this.usersService.createUser(user);
     }
 
+    // Update
+
     @Patch('id/:id')
+    @Scopes(Scope.UsersUpdate)
     updateUser(
         @Param('id') id: string,
-        @Body() user: UpdateUserDto,
+        @Body() data: UpdateUserDto,
     ) {
-        return this.usersService.updateUser(id, user);
+        return this.usersService.updateUser(id, data);
     }
 
+    @Patch('me')
+    @Scopes(Scope.UsersUpdate)
+    updateMe(
+        @User() user: SignInData,
+        @Body() data: UpdateUserDto,
+    ) {
+        return this.usersService.updateUser(user.userId, data);
+    }
+
+    // Delete
+
     @Delete('id/:id')
+    @Scopes(Scope.UsersDelete)
     deleteUser(
         @Param('id') id: string,
     ) {
         return this.usersService.deleteUser(id);
+    }
+
+    @Delete('me')
+    @Scopes(Scope.UsersDeleteMe)
+    deleteMe(
+        @User() user: SignInData,
+        @Body() data: UpdateUserDto,
+    ) {
+        return this.usersService.updateUser(user.userId, data);
     }
     
 }
